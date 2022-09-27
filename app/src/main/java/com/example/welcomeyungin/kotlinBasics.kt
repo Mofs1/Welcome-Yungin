11fadccb44a7294e37c1731f70d3169115839aa8
Mofss <meleshinnla@yahoo.com>
2022-09-27 12:26:15 +0100

    Final version

---
+++ app/src/main/java/com/example/welcomeyungin/kotlinBasics.kt
@@ -0,0 +1,35 @@
+package com.example.welcomeyungin
+
+fun main(){
+    // immutable variable
+    // TODO: add new functionality
+
+    /*
+     this
+     is a multiline
+     comment
+     */
+
+    //type string
+    val myName = "Seyi"
+    //type int 32 bit
+    var myAge = 24
+
+    val myByte: Byte = 13
+    val myShort: Short = 125
+    val myInt: Int = 123123123
+    val myLong: Long = 39_812_309_487_120_300
+
+    //booleans is used to represent logical values
+    //it can have two possible values true and false.
+    var isSunny: Boolean = true
+    isSunny = false
+
+    // Characters
+    val letterChar = 'A'
+    val digitChar = '1'
+
+
+
+    print("Hello " + myName)
+}
